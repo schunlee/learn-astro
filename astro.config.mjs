31bf917c15c800588282a3ce1776ce23ce49a374
lixun <bill.li@nexa-corp.com>
2024-07-18 15:19:11 +0800

    add preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://rainbow-haupia-e851a0.netlify.app"
+  site: "https://rainbow-haupia-e851a0.netlify.app",
+  integrations: [preact()]
 });
